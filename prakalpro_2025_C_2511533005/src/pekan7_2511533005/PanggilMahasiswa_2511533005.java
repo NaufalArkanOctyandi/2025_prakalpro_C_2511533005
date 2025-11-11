c093f57d8c23b5372b59af59850f1b4281e32563
Naufal Arkan <Naufal Arkan@LAPTOP-OLV2LUBM>
2025-11-11 11:51:59 +0700

    Praktikum Pekan7_25111533005

---
+++ prakalpro_2025_C_2511533005/src/pekan7_2511533005/PanggilMahasiswa_2511533005.java
@@ -0,0 +1,15 @@
+package pekan7_2511533005;
+
+public class PanggilMahasiswa_2511533005 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511533005 a= new Mahasiswa_2511533005 ();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println(a.getNim ());
+		System.out.println(a.getNama ());
+		a.Cetak();
+
+	}
+
+}
